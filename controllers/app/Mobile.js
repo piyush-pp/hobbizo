9f81759789827ac541a8f2ebf8926038c64d68fc
piyush-pp <31167600+piyush-pp@users.noreply.github.com>
2022-08-18 11:17:33 +0530

    Update Mobile.js

--- controllers/app/Mobile.js
+++ controllers/app/Mobile.js
@@ -4,49 +4,57 @@ const SERVICE_ID = process.env.SERVICE_ID;
 const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
 
 exports.Otp = (req, res) => {
-client.verify
-    .services(SERVICE_ID)
-    .verifications.create({
-        to: req.body.phone,
-        channel: 'sms',
-    })
-    .then(result => {
-        console.log(result)
-        res.json({
+// client.verify
+//     .services(SERVICE_ID)
+//     .verifications.create({
+//         to: req.body.phone,
+//         channel: 'sms',
+//     })
+//     .then(result => {
+//         console.log(result)
+//         res.json({
+//             status :true,
+//             code:200,
+//         })
+//     }).catch(err =>{
+//         console.log(err)
+//         res.json({
+//             status :false,
+//             code:403,
+//         })
+//     })
+     res.json({
             status :true,
             code:200,
         })
-    }).catch(err =>{
-        console.log(err)
-        res.json({
-            status :false,
-            code:403,
-        })
-    })
 };
   
 exports.Verify = (req, res) => {
-client.verify
-    .services(SERVICE_ID)
-    .verificationChecks.create({
-        to: req.body.phone,
-        code: req.body.code,
-    })
-    .then(result => {
-        console.log(result)
-        if(result.valid){
-            res.json({
-                status :true,
-                code:200,
-            })
-        }else{
-            res.json({
-                status :false,
-                code:403,
-            })
-        }
+// client.verify
+//     .services(SERVICE_ID)
+//     .verificationChecks.create({
+//         to: req.body.phone,
+//         code: req.body.code,
+//     })
+//     .then(result => {
+//         console.log(result)
+//         if(result.valid){
+//             res.json({
+//                 status :true,
+//                 code:200,
+//             })
+//         }else{
+//             res.json({
+//                 status :false,
+//                 code:403,
+//             })
+//         }
         
-    }).catch(err => {
-        console.log(err)
-    })
+//     }).catch(err => {
+//         console.log(err)
+//     })
+     res.json({
+            status :true,
+            code:200,
+        })
 };    
